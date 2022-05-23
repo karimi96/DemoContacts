f44019d21243376cc0510f5720bdf0cf5077a2cb
karimi <zmanyamyd5@gmail.com>
2022-05-23 14:10:36 +0430

    fix click listener eachc item

--- app/src/main/java/com/karimi/contacts/adapter/ContactAdapter.kt
+++ app/src/main/java/com/karimi/contacts/adapter/ContactAdapter.kt
@@ -1,15 +1,17 @@
 package com.karimi.contacts.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.AssetManager
+import android.content.res.Resources
 import android.graphics.Color
 import android.graphics.Typeface
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import android.widget.Filter.FilterResults
 import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
@@ -18,11 +20,24 @@ import com.amulyakhare.textdrawable.TextDrawable
 import com.amulyakhare.textdrawable.util.ColorGenerator
 import com.karimi.contacts.R
 import com.karimi.contacts.databinding.ListItemContactsBinding
+import com.karimi.contacts.model.User
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ContactAdapter(var list: ArrayList<String>, var context: Context) :
-    RecyclerView.Adapter<ContactAdapter.MyViewHolder>() {
+class ContactAdapter(var list: ArrayList<User>, var context: Context) :
+    RecyclerView.Adapter<ContactAdapter.MyViewHolder>() , Filterable {
 
     var rawIndex : Int = -1
+
+    lateinit var listAlfabet : ArrayList<String>
+    lateinit var pos : ArrayList<Int>
+
+    var last_position = -1
+
+    var ls: ArrayList<User> =list
+    var ll: ArrayList<User> = ls
+
+    var listColor = arrayOf(R.color.black)
 
 
     inner class MyViewHolder(binding: ListItemContactsBinding) :
@@ -38,13 +53,19 @@ class ContactAdapter(var list: ArrayList<String>, var context: Context) :
 
         init {
             itemView.setOnClickListener {
-                rawIndex = position
-                notifyDataSetChanged()
+                if (last_position == position){
+                    if (rawIndex == -1) rawIndex = position
+                    else rawIndex = -1
+                }else{
+                    rawIndex = position
+                }
+                notifyItemChanged(last_position)
+                last_position = position
+                notifyItemChanged(position)
                 Toast.makeText(context, rawIndex.toString() , Toast.LENGTH_SHORT).show()
             }
 
         }
-
     }
 
 
@@ -58,11 +79,12 @@ class ContactAdapter(var list: ArrayList<String>, var context: Context) :
 
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.name.text = list[position]
+        holder.name.text = list[position].firstName + " " + list[position].lastName
         holder.image.setImageDrawable(textDrawable(position))
-        holder.alefba.text = "A"
 
         showMoreInfo(holder , position)
+        getFirstIndex()
+        setAlfabet(holder,position)
 
     }
 
@@ -72,43 +94,38 @@ class ContactAdapter(var list: ArrayList<String>, var context: Context) :
     }
 
 
-//
-//    override fun getFilter(): Filter {
-//        return newsFilter
-//    }
-//
-//
-//
-//    fun getFilter(): Filter? {
-//        return newsFilter
-//    }
-//
-//    val newsFilter: Filter = object : Filter() {
-//        override fun performFiltering(constraint: CharSequence): FilterResults {
-//            val filterdNewList: MutableList<String> = java.util.ArrayList<String>()
-//            if (constraint == null || constraint.length == 0) {
-//                filterdNewList.addAll(list)
-//            } else {
-//                val filterPattern = constraint.toString().toLowerCase().trim { it <= ' ' }
-//                for (customer in list) {
-//                    if (list.contains(filterPattern)) filterdNewList.add(
-//
-//                    )
-//                   filterdNewList.add(list.)
-//                }
-//            }
-//            val results = FilterResults()
-//            results.values = filterdNewList
-//            results.count = filterdNewList.size
-//            return results
-//        }
-//
-//        override fun publishResults(constraint: CharSequence, results: FilterResults) {
-//            list.clear()
-//            list.addAll(results.values as java.util.ArrayList<*>)
-//            notifyDataSetChanged()
-//        }
-//    }
+
+    override fun getFilter(): Filter {
+        return newsFilter
+    }
+
+
+    val newsFilter: Filter = object : Filter() {
+        override fun performFiltering(constraint: CharSequence): FilterResults {
+            val filterdNewList: MutableList<User> = java.util.ArrayList<User>()
+
+            if (constraint == null || constraint.length == 0) {
+                filterdNewList.addAll(ls)
+            } else {
+                val filterPattern = constraint.toString().toLowerCase().trim { it <= ' ' }
+                for ( uu :User in ls) {
+                    if (uu.firstName.lowercase().contains(filterPattern)) filterdNewList.add(uu)
+                }
+            }
+            val results = FilterResults()
+            results.values = filterdNewList
+            results.count = filterdNewList.size
+            return results
+        }
+
+
+        override fun publishResults(constraint: CharSequence?, results: FilterResults) {
+            ll.clear()
+            ll.addAll(results.values as java.util.ArrayList<User>)
+            notifyDataSetChanged()
+        }
+
+    }
 
 
 
@@ -116,15 +133,16 @@ class ContactAdapter(var list: ArrayList<String>, var context: Context) :
     private fun textDrawable(pos : Int): TextDrawable? {
         val generator = ColorGenerator.MATERIAL // or use DEFAULT
         val color1 = generator.randomColor
-//        val color2 = generator.getColor("user@gmail.com")
+        val color2 = generator.getColor("user@gmail.com")
         val builder = TextDrawable.builder()
             .beginConfig()
-            .fontSize(24)
+            .fontSize(40)
+            .bold()
             .textColor(Color.WHITE)
-            .useFont(Typeface.DEFAULT)
+            .useFont(Typeface.createFromAsset(context.assets , "font/inter.ttf" ))
             .endConfig()
             .round()
-        val ic1 = builder.build(list[pos][0] + "", color1)
+        val ic1 = builder.build(list[pos].firstName[0] + "", color1)
 
         return ic1
     }
@@ -132,10 +150,11 @@ class ContactAdapter(var list: ArrayList<String>, var context: Context) :
 
    private fun showMoreInfo(holder: MyViewHolder, position: Int){
         if (rawIndex == position){
+            holder.linear1.background = null
             holder.box_cmi.visibility = View.VISIBLE
             holder.line.visibility = View.VISIBLE
+//            holder.linear.background = getDrawable(context,R.color.toi)
             holder.linear.background = getDrawable(context, R.drawable.back_call)
-            holder.linear1.background = null
         }else{
             holder.box_cmi.visibility = View.GONE
             holder.line.visibility = View.GONE
@@ -143,5 +162,148 @@ class ContactAdapter(var list: ArrayList<String>, var context: Context) :
         }
 
     }
+
+
+    fun getFirstIndex(){
+        pos = ArrayList();
+        pos.add(0)
+
+        listAlfabet = ArrayList()
+        listAlfabet.add(0 ,list[0].firstName[0].uppercase() )
+
+        var i = 1
+        while (i < list.size){
+            if(list[i].firstName[0].uppercase() in listAlfabet){
+                i++
+                continue
+            } else {
+                listAlfabet.add(list[i].firstName[0].uppercase() + "")
+                pos.add(i)
+                i++
+            }
+        }
+
+        Log.e("qqq", "getFirstIndex:  $listAlfabet" )
+    }
+
+
+
+    fun setAlfabet(holder: MyViewHolder, position: Int){
+        var i = 0
+        while (i < pos.size){
+            if (position == pos[i]) holder.alefba.text= listAlfabet[i]
+            i++
+        }
+    }
+
+
+
+
+
+
+//    override fun getFilter(): Filter {
+//        return object : Filter() {
+//            override fun performFiltering(constraint: CharSequence?): FilterResults {
+//                val charSearch = constraint.toString()
+//                if (charSearch.isEmpty()) {
+//                    countryFilterList = list
+//                } else {
+//                    val resultList = ArrayList<User>()
+//                    for (row in list) {
+//                        if (row.firstName.lowercase(Locale.ROOT).contains(charSearch.lowercase(Locale.ROOT))) {
+//                            resultList.add(row)
+//                        }
+//                    }
+//                    countryFilterList = resultList
+//                }
+//                val filterResults = FilterResults()
+//                filterResults.values = countryFilterList
+//                return filterResults
+//            }
+//
+//            @Suppress("UNCHECKED_CAST")
+//            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+//                countryFilterList = results?.values as ArrayList<User>
+//                notifyDataSetChanged()
+//            }
+//
+//        }
+//    }
+
+
+
+
+
+
+
+//    override fun getFilter(): Filter? {
+//        return newsFilter
+//    }
+//
+//    private val newsFilter: Filter = object : Filter() {
+//        override fun performFiltering(constraint: CharSequence): FilterResults {
+//            val filterdNewList: MutableList<User> = java.util.ArrayList<User>()
+//            if (constraint == null || constraint.length == 0) {
+//                filterdNewList.addAll(list)
+//            } else {
+//                val filterPattern = constraint.toString().toLowerCase().trim { it <= ' ' }
+//                for (userr : User in list) {
+//                    if (userr.firstName.toLowerCase().contains(filterPattern)) filterdNewList.add(userr)
+//                }
+//            }
+//            val results = FilterResults()
+//            results.values = filterdNewList
+//            results.count = filterdNewList.size
+//            if (results.count == 0) Toast.makeText(context, "موردی یافت نشد", Toast.LENGTH_SHORT)
+//                .show()
+//            return results
+//        }
+//
+//        override fun publishResults(constraint: CharSequence, results: FilterResults) {
+////            list.clear()
+//            countryFilterList = results?.values as ArrayList<User>
+//            notifyDataSetChanged()
+//        }
+//    }
+
+
+
+
+
+//    override fun getFilter(): Filter {
+//        return object : Filter() {
+//            override fun performFiltering(constraint: CharSequence?): FilterResults {
+//
+//
+//
+//                val charString = constraint?.toString() ?: ""
+//                if (charString.isEmpty()) photosListFiltered = photosList else {
+//                    val filteredList = ArrayList<User>()
+//                    photosList
+//                        .filter {
+//                            (it.firstName.contains(constraint!!)) or
+//                                    (it.lastName.contains(constraint))
+//
+//                        }
+//                        .forEach { filteredList.add(it) }
+//                    photosListFiltered = filteredList
+//
+//                }
+//                return FilterResults().apply { values = photosListFiltered }
+//            }
+//
+//            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+//
+//                photosListFiltered = if (results?.values == null)
+//                    ArrayList()
+//                else
+//                    results.values as ArrayList<User>
+//                notifyDataSetChanged()
+//            }
+//        }
+//    }
+
+
+
 
 }
