39b6fa29b46e45ea90af68b31b3ef06c6f74b93e
karimi <zmanyamyd5@gmail.com>
2022-05-25 18:06:10 +0430

    create DetaileActivity

---
+++ app/src/main/java/com/karimi/contacts/activity/DetaileContactActivity.kt
@@ -0,0 +1,12 @@
+package com.karimi.contacts.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karimi.contacts.R
+
+class DetaileContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detaile_contact)
+    }
+}
